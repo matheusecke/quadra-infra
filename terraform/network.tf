efb297ca2e5cc4db3853ab04eccfda72f357b749
Matheus Ecke Medeiros <matheus.ecke@skedway.com>
2026-08-14 15:21:12 -0300

    feat(terraform): add public and private subnets

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,3 +1,7 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -5,5 +9,45 @@ resource "aws_vpc" "main" {
 
   tags = merge(local.common_tags, {
     Name = "${local.name_prefix}-vpc"
+  })
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-public-a"
+  })
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-public-b"
+  })
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-private-a"
+  })
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.12.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-private-b"
   })
 }
